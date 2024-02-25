321ed1d3bc678aa30a7fe7e286824fcc7fb2e081
csmcgrl <dahapost@gmail.com>
2024-02-25 20:28:54 +0300

    urlValidation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-import 'bootstrap';
+// import 'bootstrap';
+import '../main.css';
+import * as yup from 'yup';
 
-console.log('Hello World!');
+let schema = yup.string().url();
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const input = document.getElementById('url-input').value;
+  // Проверяем, соответствует ли введенное значение схеме валидации
+  schema.isValid(input).then((valid) => {
+    if (valid) {
+      alert('Введенный URL адрес корректен');
+      // Вы можете сделать что-то еще, если URL адрес корректен
+    } else {
+      alert('Неверный URL адрес');
+      // Вы можете сделать что-то еще, если URL адрес неверный
+    }
+  });
+  // .catch((error) => {
+  //   console.log(error); // Обработка ошибки, если что-то пошло не так
+  // });
+});
