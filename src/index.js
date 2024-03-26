c6851ee68377fb2d8222a9637afef65d110b9a78
csmcgrl <dahapost@gmail.com>
2024-03-26 18:03:14 +0300

    step4.3

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const schema = yup.string().url();
 const form = document.querySelector('form');
 export const input = document.getElementById('url-input');
 const displayPosts = document.getElementsByClassName('posts')[0];
+const displayFeeds = document.getElementsByClassName('feeds')[0];
 const feedHistory = [];
 
 // yup.setLocale({
@@ -33,23 +34,39 @@ const handleInput = (inputValue) => {
         throw new Error('Network response was not ok.');
       })
       .then((data) => {
-        const postsList = createPosts();
+        const postsList = createContainer('Посты', displayPosts);
+        const feedsList = createContainer('Фиды', displayFeeds);
+
         const parser = new DOMParser();
         const doc = parser.parseFromString(data.contents, 'text/html');
+
+        const feedTitle = doc.querySelector('title');
+        const extractedFeed = extractCdataContent(feedTitle);
+
+        const feedDesc = doc.querySelector('description');
+        const extractedFeedDesc = feedDesc.childNodes[0].nodeValue;
+        console.log(feedDesc);
+        console.log(extractedFeedDesc);
+
+        const feedsItem = createFeeds(extractedFeed);
+        feedsList.appendChild(feedsItem);
+
         const items = doc.body.getElementsByTagName('item');
         let id = 1;
         for (let item of items) {
           const title = item.querySelector('title');
           const link = item.querySelector('guid').textContent;
-          const extractedText = title.textContent
-            .replace('<![CDATA[', '')
-            .replace(']]>', '');
+          const extractedText = extractCdataContent(title);
           let listItem = createOnePost(id, link, extractedText);
           postsList.appendChild(listItem);
           id++;
         }
       });
   }
+};
+
+const extractCdataContent = (cdata) => {
+  return cdata.textContent.replace('<![CDATA[', '').replace(']]>', '');
 };
 
 const handleInvalidInput = () => {
@@ -70,22 +87,21 @@ form.addEventListener('submit', function (e) {
   });
 });
 
-//Создаем на странице посты и фиды
-const createPosts = () => {
-  const postsContainer = document.createElement('div');
-  postsContainer.classList.add('card', 'border-0');
-  displayPosts.appendChild(postsContainer);
+const createContainer = (containerName, display) => {
+  const container = document.createElement('div');
+  container.classList.add('card', 'border-0');
+  display.appendChild(container);
 
-  const postsTitle = document.createElement('div');
-  postsTitle.classList.add('card-body');
-  postsTitle.innerHTML = '<h2 class="card-title h4">Посты</h2>';
-  postsContainer.appendChild(postsTitle);
+  const title = document.createElement('div');
+  title.classList.add('card-body');
+  title.innerHTML = `<h2 class="card-title h4">${containerName}</h2>`;
+  container.appendChild(title);
 
-  const postsList = document.createElement('ul');
-  postsList.classList.add('list-group', 'border-0', 'rounded-0');
-  postsContainer.appendChild(postsList);
+  const list = document.createElement('ul');
+  list.classList.add('list-group', 'border-0', 'rounded-0');
+  container.appendChild(list);
 
-  return postsList;
+  return list;
 };
 
 //функция для создания одного элемента списка постов
@@ -118,6 +134,24 @@ const createOnePost = (id, url, textContent) => {
 
   listItem.appendChild(link);
   listItem.appendChild(button);
+
+  return listItem;
+};
+
+const createFeeds = (firstText) => {
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-group-item', 'border-0', 'border-end-0');
+
+  const descriptionTitle = document.createElement('h3');
+  descriptionTitle.classList.add('h6', 'm-0');
+  descriptionTitle.textContent = firstText;
+
+  const descriptionText = document.createElement('p');
+  descriptionText.classList.add('m-0', 'small', 'text-black-50');
+  descriptionText.textContent =
+    'This is a constantly updating lorem ipsum feed';
+
+  listItem.append(descriptionTitle, descriptionText);
 
   return listItem;
 };
